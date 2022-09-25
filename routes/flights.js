3f173bd835e4a48bd9f49a2360dcea929af32e0c
Ronnakon <ronnakon_nisaensat@cmu.ac.th>
2022-09-25 16:09:48 +0700

    flight api filter

--- routes/flights.js
+++ routes/flights.js
@@ -2,49 +2,21 @@ const express = require('express');
 const router = express.Router();
 const data = require('../ForDatas/Flights.json');
 
-router.get('/', (req, res) => {
-    res.json(data);
-});
 // router.get('/', (req, res) => {
-//     const filteredProds = data.filter(product => product.name.startsWith(req.query.name))
-//     res.json(filteredProds)
+//     res.json(data);
 // });
 
-router.get('/:id', (req, res) => {
-    const flightId = Number.parseInt(req.params.id);
-    const flight = data.find((flight) => flight.id === flightId);
-    res.json(flight);
+router.use('/', (req, res, next) => {
+    const filters = req.query;
+    const filteredFlights = data.filter(flight => {
+        let isValid = true;
+        for (key in filters) {
+            // console.log(key, flight[key], filters[key]);
+            isValid = isValid && flight[key] == filters[key];
+        }
+        return isValid;
+    });
+    res.send(filteredFlights);
 });
-
-// let currentProductId = 9;
-// router.post('/', (req, res) => {
-//     const { name, imageURL, type } = req.body;
-//     const product = {
-//         id: ++currentProductId,
-//         name,
-//         imageURL,
-//         type
-//     };
-//     data.push(product);
-//     res.json(product);
-// });
-
-// router.put('/:id', (req, res) => {
-//     const { name, imageURL, type } = req.body;
-//     const productId = Number.parseInt(req.params.id);
-//     const product = data.find((product) => product.id === productId);
-
-//     product.name = name;
-//     product.imageURL = imageURL;
-//     product.type = type;
-//     res.json(product);
-// });
-
-// router.delete('/:id', (req, res) => {
-//     const productId = Number.parseInt(req.params.id);
-//     const productIndex = data.findIndex((product) => product.id === productId);
-//     data.splice(productIndex, 1);
-//     res.sendStatus(204);
-// });
 
 module.exports = router;
