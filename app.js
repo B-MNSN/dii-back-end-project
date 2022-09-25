0fc8a79c3f02653f18ab0bce57d379b12b108abd
Ronnakon <ronnakon_nisaensat@cmu.ac.th>
2022-09-25 14:19:21 +0700

    Add data folder

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const app = express();
+const flightsRoutes = require('./routes/flights');
+const morgan = require('morgan');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(morgan('tiny'));
+app.use('/flights', flightsRoutes);
+app.use('/books', flightsRoutes);
+
+
+app.listen(8000, () => {
+    console.log('Listening on port 8000');
+})
+
+
+// GET /flights     => [{ id: 1, destinationAirline: 'Thai', departureDate: '2022-09-24T08:00:00Z', destination: 'BKK' }]
+// /flights?destination=BKK
+// POST /flights
+// PUT /flights/:id
+// DELETE /flights/:id
+
+// Express passPort
---
+++ routes/flights.js
@@ -0,0 +1,50 @@
+const express = require('express');
+const router = express.Router();
+const { data } = require('../ForDatas/Flights.json');
+
+router.get('/', (req, res) => {
+    res.send(data);
+});
+router.get('/', (req, res) => {
+    const filteredProds = data.filter(product => product.name.startsWith(req.query.name))
+    res.json(filteredProds)
+});
+
+router.get('/:id', (req, res) => {
+    const productId = Number.parseInt(req.params.id);
+    const product = data.find((product) => product.id === productId);
+    res.json(product);
+});
+
+let currentProductId = 9;
+router.post('/', (req, res) => {
+    const { name, imageURL, type } = req.body;
+    const product = {
+        id: ++currentProductId,
+        name,
+        imageURL,
+        type
+    };
+    data.push(product);
+    res.json(product);
+});
+
+router.put('/:id', (req, res) => {
+    const { name, imageURL, type } = req.body;
+    const productId = Number.parseInt(req.params.id);
+    const product = data.find((product) => product.id === productId);
+
+    product.name = name;
+    product.imageURL = imageURL;
+    product.type = type;
+    res.json(product);
+});
+
+router.delete('/:id', (req, res) => {
+    const productId = Number.parseInt(req.params.id);
+    const productIndex = data.findIndex((product) => product.id === productId);
+    data.splice(productIndex, 1);
+    res.sendStatus(204);
+});
+
+module.exports = router;
