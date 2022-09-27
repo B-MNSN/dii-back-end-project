e1a4ce447b2e1e04a6393ca4cd8b3e32178d8dc0
Ronnakon <ronnakon_nisaensat@cmu.ac.th>
2022-09-27 14:21:48 +0700

    grammar key fix

--- routes/books.js
+++ routes/books.js
@@ -15,42 +15,42 @@ router.get('/', (req, res) => {
     res.json(filteredFlights);
 });
 
-router.get('/:id', (req, res) => {
-    const productId = Number.parseInt(req.params.id);
-    const product = data.find((product) => product.id === productId);
-    res.json(product);
+router.get('/:UID', (req, res) => {
+    const UId = req.params.UID;
+    const book = data.find((book) => book.UID === UId);
+    res.json(book);
 });
 
 
-router.post('/', (req, res) => {
-    const { name, imageURL, type } = req.body;
-    const product = {
-        id: ++currentProductId,
-        name,
-        imageURL,
-        type
-    };
-    data.push(product);
-    res.json(product);
-});
+// router.post('/', (req, res) => {
+//     const { name, imageURL, type } = req.body;
+//     const product = {
+//         id: ++currentProductId,
+//         name,
+//         imageURL,
+//         type
+//     };
+//     data.push(product);
+//     res.json(product);
+// });
 
 
-router.put('/:id', (req, res) => {
-    const { name, imageURL, type } = req.body;
-    const productId = Number.parseInt(req.params.id);
-    const product = data.find((product) => product.id === productId);
+// router.put('/:id', (req, res) => {
+//     const { name, imageURL, type } = req.body;
+//     const productId = Number.parseInt(req.params.id);
+//     const product = data.find((product) => product.id === productId);
 
-    product.name = name;
-    product.imageURL = imageURL;
-    product.type = type;
-    res.json(product);
-});
+//     product.name = name;
+//     product.imageURL = imageURL;
+//     product.type = type;
+//     res.json(product);
+// });
 
-router.delete('/:id', (req, res) => {
-    const productId = Number.parseInt(req.params.id);
-    const productIndex = data.findIndex((product) => product.id === productId);
-    data.splice(productIndex, 1);
-    res.sendStatus(204);
-});
+// router.delete('/:id', (req, res) => {
+//     const productId = Number.parseInt(req.params.id);
+//     const productIndex = data.findIndex((product) => product.id === productId);
+//     data.splice(productIndex, 1);
+//     res.sendStatus(204);
+// });
 
 module.exports = router;
